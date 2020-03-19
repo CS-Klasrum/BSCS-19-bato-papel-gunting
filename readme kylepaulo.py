36f1809f842158bf1a70ecb1778116ede9b7fa0b
paulo8080 <61894047+paulo8080@users.noreply.github.com>
2020-03-19 23:21:50 +0800

    readme kylepaulo.py

---
+++ readme kylepaulo.py
@@ -0,0 +1,30 @@
+# BSCS-19-bato,papel,gunting
+
+kyle paulo domingo
+
+## Started by
+- kyle paulo domingo Batch  2019 - 2020 
+
+## Pre-requisites
+- Microsoft DotNet Framework 4.5 or higher
+- Microsoft Visual C++ Runtime 2011
+- pyroid 3
+
+## Up and Running
+- pydroid 3 on android only
+- bato,papel,gunting hand game aginst computer
+-you choose the three options you want to choose
+-the computer also chooses what it wants to choose
+-then program declare if draw,win or you lose
+
+## References
+- https://pythonistaplanet.com/rock-paper-scissors-game-using-python-tkinter/
+
+## Contibuting to BSCS-18-bato,papel,gunting
+To contribute to BSCS-18-bato,papel,gunting, make sure to give a star and forked this repository.
+
+Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## License
+The `BSCS-18-bato,papel,gunting` is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
+
